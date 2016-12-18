8ccba4e3dbe0387df875dc7c29b50f79826847aa
oszust <oszust@memleak.pl>
2016-12-19 00:07:45 +0100

    Moved create user url to constants

--- app/containers/CreateUser/constants.js
+++ app/containers/CreateUser/constants.js
@@ -10,3 +10,5 @@ export const CREATE_USER_SUCCESS = 'app/CreateUser/CREATE_USER_SUCCESS';
 export const CREATE_USER_ERROR = 'app/CreateUser/CREATE_USER_ERROR';
 
 export const CREATE_USER_API_URL = `${API_ROOT}/user`;
+
+export const CREATE_USER_URL = '/user/create';
