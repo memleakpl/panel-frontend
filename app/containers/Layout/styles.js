aac3fe2a343c18df4e06fbf9294345d270f654a0
oszust <oszust@memleak.pl>
2016-12-22 00:25:05 +0100

    Changed hex colors to imports from material-ui colors

--- app/containers/Layout/styles.js
+++ app/containers/Layout/styles.js
@@ -1,11 +1,11 @@
 /**
  * Created by oszust on 18.12.16.
  */
-
+import { blue100, blueGrey50, white, brown100 } from 'material-ui/styles/colors';
 
 export const activeLinkStyle = {
   display: 'block',
-  backgroundColor: '#BBDEFB',
+  backgroundColor: blue100,
 };
 export const mainDivStyle = {
   display: 'flex',
@@ -13,7 +13,7 @@ export const mainDivStyle = {
   flexDirection: 'row',
   padding: '20px auto',
   minHeight: '100vh',
-  backgroundColor: '#ECEFF1',
+  backgroundColor: blueGrey50,
 };
 export const listItemStyle = {
   WebkitAppearance: 'none', // workaround of bug: https://github.com/callemall/material-ui/issues/4008
@@ -25,8 +25,8 @@ export const linkStyle = {
 export const listStyle = {
   flex: 0.13,
   position: 'sticky',
-  boxShadow: '5px 5px 8px #D7CCC8',
-  backgroundColor: '#FFFFFF',
+  boxShadow: `5px 5px 8px ${brown100}`,
+  backgroundColor: white,
 };
 export const childrenDivStyle = {
   flex: 0.8,
