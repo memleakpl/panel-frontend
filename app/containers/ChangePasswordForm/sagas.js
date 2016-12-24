f025e22c929af9f37a8aa816a78e443f92f4a15e
oszust <oszust@memleak.pl>
2016-12-24 16:10:24 +0100

    Moved changed password success notification to saga

--- app/containers/ChangePasswordForm/sagas.js
+++ app/containers/ChangePasswordForm/sagas.js
@@ -4,7 +4,7 @@
 import { call, put, select } from 'redux-saga/effects';
 import { takeLatest } from 'redux-saga';
 import Notifications from 'react-notification-system-redux';
-import { CHANGE_PASSWORD_API_URL, CHANGE_PASSWORD_REQUEST } from './constants';
+import { CHANGE_PASSWORD_API_URL, CHANGE_PASSWORD_REQUEST, CHANGE_PASSWORD_SUCCESS } from './constants';
 import { CHANGE_PASSWORD_SUCCESS_NOTIFICATION } from './notifications';
 import selectChangePasswordForm from './selectors';
 import { changePasswordError, changePasswordSuccess } from './actions';
@@ -28,16 +28,23 @@ function* changePassword() {
     const { oldPassword, newPassword } = yield select(selectChangePasswordForm());
     yield call(callChangePassword, oldPassword, newPassword);
     yield put(changePasswordSuccess());
-    yield put(Notifications.success(CHANGE_PASSWORD_SUCCESS_NOTIFICATION));
   } catch (e) {
     yield put(changePasswordError());
   }
+}
+export function* notifyChangePasswordSuccess() {
+  yield put(Notifications.success(CHANGE_PASSWORD_SUCCESS_NOTIFICATION));
 }
 export function* changePasswordSaga() {
   yield* takeLatest(CHANGE_PASSWORD_REQUEST, changePassword);
 }
 
+export function* notifyChangePasswordSuccessSaga() {
+  yield* takeLatest(CHANGE_PASSWORD_SUCCESS, notifyChangePasswordSuccess);
+}
+
 // All sagas to be loaded
 export default [
   changePasswordSaga,
+  notifyChangePasswordSuccessSaga,
 ];
