1cf174815a11e36c8ea065c6e906001afd043424
Marcin Moskal <mmos@memleak.pl>
2016-12-27 20:39:51 +0100

    Remove empty CreateGroup container tests

--- app/containers/CreateGroup/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-import expect from 'expect';
-import createGroupReducer from '../reducer';
-import { fromJS } from 'immutable';
-
-describe('createGroupReducer', () => {
-  it('returns the initial state', () => {
-    expect(createGroupReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
