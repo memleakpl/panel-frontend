5b84d764d07b7719fc219de5552552393d3c34e6
Marcin Moskal <mmos@memleak.pl>
2017-01-06 11:09:59 +0100

    Add Card at the top level container

--- app/containers/CreateUser/index.js
+++ app/containers/CreateUser/index.js
@@ -7,29 +7,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
-import selectCreateUser from './selectors';
+
+import { Card, CardTitle } from 'material-ui/Card';
+
 import { createUser } from './actions';
-import UserForm from '../UserForm';
 import messages from './messages';
+import selectCreateUser from './selectors';
+
+import { CARD_STYLE } from '../../styles';
+import UserForm from '../UserForm';
 
 export class CreateUser extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   static propTypes = {
     dispatch: React.PropTypes.func,
   };
+
   constructor() {
     super();
     this.onSubmit = this.onSubmit.bind(this);
   }
+
   onSubmit() {
     this.props.dispatch(createUser());
   }
+
   render() {
     return (
-      <UserForm
-        header={<FormattedMessage {...messages.header} />}
-        button={<FormattedMessage {...messages.create} />}
-        onSubmit={this.onSubmit}
-      />
+      <Card style={CARD_STYLE}>
+        <UserForm
+          header={<CardTitle title={<FormattedMessage {...messages.header} />} />}
+          button={<FormattedMessage {...messages.create} />}
+          onSubmit={this.onSubmit}
+        />
+      </Card>
     );
   }
 }
--- app/containers/EditUser/index.js
+++ app/containers/EditUser/index.js
@@ -7,11 +7,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
-import selectEditUser from './selectors';
-import UserForm from '../UserForm';
+
+import { Card, CardTitle } from 'material-ui/Card';
+
 import { getUser, editUser } from './actions';
 import messages from './messages';
+import selectEditUser from './selectors';
 
+import { CARD_STYLE } from '../../styles';
+import UserForm from '../UserForm';
 
 export class EditUser extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   static propTypes = {
@@ -19,24 +23,30 @@ export class EditUser extends React.PureComponent { // eslint-disable-line react
     params: React.PropTypes.shape({
       username: React.PropTypes.string.isRequired,
     }).isRequired,
-  }
+  };
+
   constructor() {
     super();
     this.onSubmit = this.onSubmit.bind(this);
   }
+
   componentDidMount() {
     this.props.dispatch(getUser(this.props.params.username));
   }
+
   onSubmit() {
     this.props.dispatch(editUser());
   }
+
   render() {
     return (
-      <UserForm
-        header={<FormattedMessage {...messages.header} />}
-        button={<FormattedMessage {...messages.save} />}
-        onSubmit={this.onSubmit}
-      />
+      <Card style={CARD_STYLE}>
+        <UserForm
+          header={<CardTitle title={<FormattedMessage {...messages.header} />} />}
+          button={<FormattedMessage {...messages.save} />}
+          onSubmit={this.onSubmit}
+        />
+      </Card>
     );
   }
 }
