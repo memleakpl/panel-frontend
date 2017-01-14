85011601817c4fd3f0efaa6881da85b4c6751b66
Marcin Moskal <mmos@memleak.pl>
2017-01-14 12:23:03 +0100

    Language fixes

--- app/containers/UserMembership/messages.js
+++ app/containers/UserMembership/messages.js
@@ -8,22 +8,22 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   getGroupsErrorMessage: {
     id: 'app.components.GroupsTable.getGroupsErrorMessage',
-    defaultMessage: 'Cannot get groups list from server.',
+    defaultMessage: 'Cannot get groups list from the server.',
   },
   joinGroupErrorNotificationTitle: {
     id: 'app.components.GroupTable.joinGroupErrorNotificationTitle',
-    defaultMessage: 'Failed to add user "{user}" to group "{group}".',
+    defaultMessage: 'Failed to add user "{user}" to the group "{group}".',
   },
   joinGroupSuccessNotificationTitle: {
     id: 'app.components.GroupTable.joinGroupSuccessNotificationTitle',
-    defaultMessage: 'User "{user}" has been added to group "{group}".',
+    defaultMessage: 'User "{user}" has been added to the group "{group}".',
   },
   leaveGroupErrorNotificationTitle: {
     id: 'app.components.GroupTable.leaveGroupErrorNotificationTitle',
-    defaultMessage: 'Failed to remove user "{user}" from group "{group}".',
+    defaultMessage: 'Failed to remove user "{user}" from the group "{group}".',
   },
   leaveGroupSuccessNotificationTitle: {
     id: 'app.components.GroupTable.leaveGroupSuccessNotificationTitle',
-    defaultMessage: 'User "{user}" has been removed from group "{group}".',
+    defaultMessage: 'User "{user}" has been removed from the group "{group}".',
   },
 });
