969e85a3585071d4df901fbd0c03130ae7880e64
Marcin Moskal <mmos@memleak.pl>
2017-01-14 11:47:38 +0100

    Reset admin state on LOGIN/LOGOUT actions

--- app/containers/Layout/reducer.js
+++ app/containers/Layout/reducer.js
@@ -6,8 +6,10 @@
 
 import { fromJS } from 'immutable';
 import {
+  LOGOUT,
   SET_ADMIN,
 } from './constants';
+import { LOGIN } from '../LoginForm/constants';
 
 const initialState = fromJS({
   admin: undefined,
@@ -15,6 +17,9 @@ const initialState = fromJS({
 
 function layoutReducer(state = initialState, action = null) {
   switch (action.type) {
+    case LOGIN:
+    case LOGOUT:
+      return initialState;
     case SET_ADMIN:
       return state.set('admin', action.value);
     default:
